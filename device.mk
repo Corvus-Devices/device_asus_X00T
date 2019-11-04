8d7fe7adfcd671cef7e8c6fa3e6ba6646e5fc08d
Sanjay Singh <sisanj@codeaurora.org>
2019-11-04 12:06:45 +0530

    X00T: Override "vendor.vidc.enc.disable.pq" property
    Override the propery "vendor.vidc.enc.disable.pq" in
    the device make file so that it will be part of vendor.prop
    for the targets that doesn't need pq.
    
    Change-Id: Ib819a0b8692dc68ceeaba364121c46ad6f37a4c5
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- device.mk
+++ device.mk
@@ -290,6 +290,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.vidc.enc.disable.pq=true
+
 # Netutils
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.0 \
