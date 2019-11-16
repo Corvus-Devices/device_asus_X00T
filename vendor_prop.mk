2efa2ce449bcc701c608921a88b1346be3607382
Genkzsz11 <Genkzsz11@gmail.com>
2019-11-16 20:48:28 +0700

    X00T: add more debug for ims

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -242,7 +242,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
    persist.dbg.ims_volte_enable=1 \
    persist.dbg.volte_avail_ovr=1 \
    persist.dbg.vt_avail_ovr=1 \
-   persist.dbg.wfc_avail_ovr=1
+   persist.dbg.wfc_avail_ovr=1 \
+   persist.ims.enableADBLogs=1 \
+   persist.ims.enableDebugLogs=1
 
 # Radio Options
 PRODUCT_PROPERTY_OVERRIDES += \
