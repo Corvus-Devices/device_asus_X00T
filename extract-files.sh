PixelExperience-Devices/device_asus_X00TD

X00T: build with BOARD_VNDK_VERSION=current
Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

extract-files.sh
@@ -64,4 +64,8 @@ setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" false "${CLEAN_VENDOR}"
 
 extract "${MY_DIR}/proprietary-files.txt" "${SRC}" ${KANG} --section "${SECTION}"
 
+DEVICE_BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
+
+patchelf --remove-needed libandroid.so "$DEVICE_BLOB_ROOT"/vendor/lib/libmpbase.so
+
 "${MY_DIR}/setup-makefiles.sh"

Corvus-Devices/device_asus_X00T

Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

extract-files.sh
@@ -100,6 +100,9 @@ patchelf --remove-needed libsoftkeymasterdevice.so "$DEVICE_BLOB_ROOT"/vendor/li
 patchelf --remove-needed libsoftkeymaster.so "$DEVICE_BLOB_ROOT"/vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so
 patchelf --remove-needed libkeymaster_messages.so "$DEVICE_BLOB_ROOT"/vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so
 
+patchelf --remove-needed libandroid.so "$DEVICE_BLOB_ROOT"/vendor/lib/libmpbase.so
+
 fi
 
+DEVICE_BLOB_ROOT="$DU_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
 "${MY_DIR}/setup-makefiles.sh"